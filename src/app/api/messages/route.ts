caf2accd230165f60911a8c251ec81d1934be5b7
GeorgeGento <51440944+GeorgeGento@users.noreply.github.com>
2023-10-28 12:35:52 +0300

    Update maxDuration

--- src/app/api/messages/route.ts
+++ src/app/api/messages/route.ts
@@ -6,7 +6,7 @@ import { db } from "@/lib/db";
 
 const MESSAGES_BATCH = 10;
 
-export const maxDuration = 300;
+export const maxDuration = 10;
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
